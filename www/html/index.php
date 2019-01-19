59b8c0fba025c51eaf165f3795ea61a030a161d0
vincent <digitalsin@gmx.com>
2019-01-19 20:22:00 +0300

    Initial: PHP-NGINX CONFIG. Working.

---
+++ www/html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx - PHP!</title>
+</head>
+<body>
+
+<h1>Welcome to nginx - PHP!</h1>
+For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.
+
+<em>Thank you for using nginx.</em>
+<p>Now, have some <b>PHP</b>
+<p>Yeah! yeah! yeah!<p>
+<?php phpinfo(); ?>
+</body>
+</html>
